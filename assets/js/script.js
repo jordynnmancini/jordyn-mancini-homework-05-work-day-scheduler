a81996e34b8fb19ffdcfc9ba63be930640e52cc6
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-02-27 14:38:09 -0500

    updated comments on all files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
-// using Moment to show the current date at the top of the screen
+// using Moment Library to show the current date at the top of the screen
 const currentDayEl = $("#currentDay"); 
 
 var currentDay = moment().format("MMMM Do YYYY"); 
 currentDayEl.text(currentDay); 
 
-// save input in time blocks to Local Storage 
+// save input in time blocks to Local Storage when user clicks the "Save Button"
 const saveBtnEl = $(".saveBtn"); 
 
 saveBtnEl.on("click", function() {
     var input = $(this).siblings("textarea").val(); 
+    // by pulling the ID from the div parent element & storing in a variable, it allows that item to be stored with a unique name in Local Storage based on which timeblock it was entered into
     var storageKey = $(this).parent().attr("id"); 
     localStorage.setItem(storageKey, JSON.stringify(input));
     
 });
 
-// load data from Local Storage when user comes back to this site & show in the corresponding time block
+// load data from Local Storage when user comes back to this page & show in the corresponding time block
 $("#hour-9 textarea").val(JSON.parse(localStorage.getItem("hour-9")));
 $("#hour-10 textarea").val(JSON.parse(localStorage.getItem("hour-10")));
 $("#hour-11 textarea").val(JSON.parse(localStorage.getItem("hour-11")));
@@ -27,10 +28,12 @@ $("#hour-17 textarea").val(JSON.parse(localStorage.getItem("hour-17")));
 
 // color code time blocks based on time of day 
 var currentHour = moment().hours(); 
-
+    //note: the Moment Library uses military time
+    
 $(".time-block").each(function() {
+    // for each time-block, we are going to store the number from the ID (i.e., "9" from ID "hour-9") into the blockHour variable
     var blockHour = parseInt($(this).attr("id").split("-")[1]);
-
+    // we will then compare blockHour to currentHour to determine how to color code that block 
     if(currentHour < blockHour) {
         $(this).addClass("future"); 
 
